37f20c7ce3ec66b0f3bfa490f15728c6a368a32d
HN721 <119390246+HN721@users.noreply.github.com>
2025-01-03 13:08:10 +0700

    joinGroup

--- routes/grouRoute.js
+++ routes/grouRoute.js
@@ -2,15 +2,15 @@ const express = require("express");
 
 const Group = require("../models/Group");
 const groupRouter = express.Router();
-
-groupRouter.post("/", async () => {
+const { protect, isAdmin } = require("../middlewares/auth");
+groupRouter.post("/", protect, isAdmin, async (req, res) => {
   try {
     const { name, description } = req.body;
     const group = await Group.create({
       name,
       description,
-      //   admin: req.user._id,
-      // members:[req.user._id]
+      admin: req.user._id,
+      members: [req.user._id],
     });
     const populate = await Group.findById(group._id)
       .populate("admin", "username email")
@@ -20,5 +20,29 @@ groupRouter.post("/", async () => {
     res.status(400).json({ message: err.message });
   }
 });
-
+groupRouter.get("/get-all", protect, async (req, res) => {
+  try {
+    const groups = await Group.find({})
+      .populate("admin", "username email")
+      .populate("members", "username email");
+    res.status(200).json({ groups });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+//join
+groupRouter.get("/:groupId/join", protect, async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.groupId);
+    if (!group) {
+      return res.status(404).json({ message: "Group not found" });
+    }
+    if (group.members.includes(req.user._id)) {
+      return res.status(400).json({ message: "Already joined" });
+    }
+    group.members.push(req.user._id);
+    await group.save();
+    res.status(200).json({ message: "Joined group successfully" });
+  } catch (error) {}
+});
 module.exports = groupRouter;
